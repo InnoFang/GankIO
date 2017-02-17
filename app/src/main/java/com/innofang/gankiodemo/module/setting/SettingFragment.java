58e0f4d1e030211cd6e670014bfa1bd88378a38b
InnoFang <q984941707@qq.com>
2017-02-17 20:09:14 +0800

    优化

--- app/src/main/java/com/innofang/gankiodemo/module/setting/SettingFragment.java
+++ app/src/main/java/com/innofang/gankiodemo/module/setting/SettingFragment.java
@@ -3,17 +3,21 @@ package com.innofang.gankiodemo.module.setting;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.Switch;
 
+import com.bumptech.glide.Glide;
 import com.innofang.gankiodemo.R;
 import com.innofang.gankiodemo.module.setting.about.AboutFragment;
 import com.innofang.gankiodemo.utils.ToastUtil;
+
+import jp.wasabeef.glide.transformations.BlurTransformation;
+import jp.wasabeef.glide.transformations.CropCircleTransformation;
 
 /**
  * Author: Inno Fang
@@ -34,6 +38,19 @@ public class SettingFragment extends Fragment implements View.OnClickListener{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_setting, container, false);
+        ImageView settingBg = (ImageView) view.findViewById(R.id.setting_bg);
+        ImageView icon = (ImageView) view.findViewById(R.id.app_icon);
+        // 将背景模糊处理
+        Glide.with(getActivity())
+                .load(R.drawable.default_nav_img)
+                .bitmapTransform(new BlurTransformation(getActivity()))
+                .into(settingBg);
+        // 将图标圆角处理
+        Glide.with(getActivity())
+                .load(R.mipmap.ic_launcher)
+                .bitmapTransform(new CropCircleTransformation(getActivity()))
+                .into(icon);
+
         Switch modeSwitcher = (Switch) view.findViewById(R.id.switch_mode);
         modeSwitcher.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -47,7 +64,8 @@ public class SettingFragment extends Fragment implements View.OnClickListener{
     }
 
     private void setDayNightMode(boolean isChecked) {
-        if (isChecked){
+        ToastUtil.showToast("这个坏了，还没修好~  ╮(╯▽╰)╭ ");
+        /*if (isChecked){
             ToastUtil.showToast("夜间模式已打开");
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         } else {
@@ -55,7 +73,7 @@ public class SettingFragment extends Fragment implements View.OnClickListener{
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         }
         getActivity().getWindow().setWindowAnimations(R.style.WindowAnimationFadeInOut);
-        getActivity().recreate();
+        getActivity().recreate();*/
     }
 
     @Override
