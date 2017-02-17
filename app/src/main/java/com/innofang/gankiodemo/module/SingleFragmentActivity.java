25d8274b77b24bb5ae4797a310009fe74a5e1565
InnoFang <q984941707@qq.com>
2017-02-17 15:12:37 +0800

    优化

--- app/src/main/java/com/innofang/gankiodemo/module/SingleFragmentActivity.java
+++ app/src/main/java/com/innofang/gankiodemo/module/SingleFragmentActivity.java
@@ -8,6 +8,7 @@ import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 
@@ -17,7 +18,7 @@ import android.view.Window;
  * Description:
  */
 
-public abstract class SingleFragmentActivity extends BaseActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     private FragmentManager mFragmentManager;
     private Fragment mFragment;
@@ -34,6 +35,13 @@ public abstract class SingleFragmentActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         getWindow().requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS);
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21){
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    |View .SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(getLayoutResId());
 
         mFragmentManager = getSupportFragmentManager();
