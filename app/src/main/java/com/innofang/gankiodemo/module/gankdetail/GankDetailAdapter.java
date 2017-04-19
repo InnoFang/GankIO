0a20a0a882bed813ef853a4857ad20876166329b
InnoFang <q984941707@qq.com>
2017-04-19 19:37:03 +0800

    update UI

--- app/src/main/java/com/innofang/gankiodemo/module/gankdetail/GankDetailAdapter.java
+++ app/src/main/java/com/innofang/gankiodemo/module/gankdetail/GankDetailAdapter.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
@@ -186,7 +188,10 @@ public class GankDetailAdapter extends RecyclerView.Adapter<GankDetailAdapter.Ga
                 }
             }
             // 为每个item添加动画
-            itemView.startAnimation(AnimationUtils.loadAnimation(mContext, R.anim.anim_item));
+            Animation animation = AnimationUtils.loadAnimation(mContext, R.anim.anim_item);
+            animation.setStartOffset(position * 250);
+            animation.setInterpolator(new AccelerateInterpolator());
+            itemView.startAnimation(animation);
         }
     }
 
