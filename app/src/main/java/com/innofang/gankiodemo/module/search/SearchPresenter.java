2bfa9f584bab1e3c22d87c5d52557424f80fc6e9
InnoFang <q984941707@qq.com>
2017-05-03 14:08:03 +0800

    Use Retrofit2 instead of OkHttp3

--- app/src/main/java/com/innofang/gankiodemo/module/search/SearchPresenter.java
+++ app/src/main/java/com/innofang/gankiodemo/module/search/SearchPresenter.java
@@ -4,14 +4,8 @@ import android.text.TextUtils;
 import android.util.Log;
 
 import com.innofang.gankiodemo.bean.GankSearch;
-import com.innofang.gankiodemo.constant.URL;
-import com.innofang.gankiodemo.http.LoadingCallback;
-import com.innofang.gankiodemo.http.RemoteManager;
-import com.innofang.gankiodemo.utils.JSONParser;
+import com.innofang.gankiodemo.http.Api;
 
-import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
@@ -35,32 +29,13 @@ public class SearchPresenter implements SearchContract.Presenter {
 
     @Override
     public void requestSearchResult(final String query) {
-        if (TextUtils.isEmpty(query)){
+        if (TextUtils.isEmpty(query)) {
             mView.setLoadingState(false);
             return;
         }
         mView.setLoadingState(true);
-        Observable.create(new ObservableOnSubscribe<GankSearch>() {
-            @Override
-            public void subscribe(final ObservableEmitter<GankSearch> e) throws Exception {
-                RemoteManager.getInstance().asyncRequest(URL.getSearchData(query), new LoadingCallback() {
-                    @Override
-                    public void onUnavailable() {
-                        mView.setLoadingState(false);
-                        mView.showEmptyOrError("干货查询失败");
-                    }
-
-                    @Override
-                    public void onLoad(String json) {
-                        GankSearch  gankSearch = JSONParser.parseJson(json, GankSearch.class);
-                        if (null != gankSearch){
-                            Log.i(TAG, "onLoad: ganksearch " + gankSearch.toString());
-                            e.onNext(gankSearch);
-                        }
-                    }
-                });
-            }
-        }).subscribeOn(Schedulers.io())
+        Api.getGankService().getGankSearch(query, 50, 1)
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Observer<GankSearch>() {
                     @Override
@@ -70,7 +45,7 @@ public class SearchPresenter implements SearchContract.Presenter {
 
                     @Override
                     public void onNext(GankSearch value) {
-                        if (null != value.getResults()){
+                        if (null != value.getResults()) {
                             Log.i(TAG, "onNext: gankResult = " + value.getResults().toString());
                             mView.showSearchResult(value.getResults());
                         }
